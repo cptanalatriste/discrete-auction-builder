bc52061e02d9fd6df3152291dae4580144227e7f
Carlos G. Gavidia <cgavidiac@gmail.com>
2019-03-17 12:26:54 +0000

    Code for obtaining utilities

--- gamebuilder.py
+++ gamebuilder.py
@@ -1,4 +1,5 @@
 import itertools
+from abc import ABC, abstractmethod
 
 
 class PlayerSpecification(object):
@@ -9,6 +10,37 @@ class PlayerSpecification(object):
 
     def get_pure_strategies(self):
         return itertools.product(self.player_actions, repeat=len(self.player_types))
+
+
+class BayesianGame(ABC):
+
+    def __init__(self, player_specification, opponent_specification):
+        self.player_specification = player_specification
+        self.opponent_specification = opponent_specification
+
+    def get_expected_utilities(self, strategy_profile):
+        player_strategy, opponent_strategy = strategy_profile
+        types_iterator = itertools.product(self.player_specification.player_types,
+                                           self.opponent_specification.player_types)
+
+        expected_player_utility, expected_opponent_utility = 0.0, 0.0
+        for player_type, opponent_type in types_iterator:
+            probability = self.get_types_probability(player_type, opponent_type)
+            player_utility, opponent_utility = self.get_utility(player_type, player_strategy, opponent_type,
+                                                                opponent_strategy)
+
+            expected_player_utility += probability * player_utility
+            expected_opponent_utility += probability * opponent_utility
+
+        return expected_player_utility, expected_opponent_utility
+
+    @abstractmethod
+    def get_types_probability(self, player_type, opponent_type):
+        pass
+
+    @abstractmethod
+    def get_utility(self, player_type, player_strategy, opponent_type, opponnet_strategy):
+        pass
 
 
 if __name__ == "__main__":
--- gamebuilder_test.py
+++ gamebuilder_test.py
@@ -1,15 +1,82 @@
 import unittest
 
 from gamebuilder import PlayerSpecification
+from gamebuilder import BayesianGame
+
+
+class SampleGame(BayesianGame):
+
+    def __init__(self):
+        super(SampleGame, self).__init__(
+            player_specification=PlayerSpecification(player_types=[1, 2], player_actions=["U", "D"]),
+            opponent_specification=PlayerSpecification(player_types=[1, 2],
+                                                       player_actions=["R",
+                                                                       "L"]))
+
+        self.type_probability = {(1, 1): .3,
+                                 (1, 2): .1,
+                                 (2, 1): .2,
+                                 (2, 2): .4}
+
+        self.utility_values = {(1, "U", 1, "L"): (2, 0),
+                               (1, "U", 2, "L"): (2, 2),
+                               (2, "U", 1, "L"): (2, 2),
+                               (2, "U", 2, "L"): (2, 1),
+                               (1, "U", 1, "R"): (0, 2),
+                               (1, "U", 2, "R"): (0, 3),
+                               (2, "U", 1, "R"): (0, 0),
+                               (2, "U", 2, "R"): (0, 0),
+                               (1, "D", 1, "L"): (0, 2),
+                               (1, "D", 2, "L"): (3, 0),
+                               (2, "D", 1, "L"): (0, 0),
+                               (2, "D", 2, "L"): (0, 0),
+                               (1, "D", 1, "R"): (2, 0),
+                               (1, "D", 2, "R"): (1, 1),
+                               (2, "D", 1, "R"): (1, 1),
+                               (2, "D", 2, "R"): (1, 2)}
+
+    def get_types_probability(self, player_type, opponent_type):
+        return self.type_probability[(player_type, opponent_type)]
+
+    def get_utility(self, player_type, player_strategy, opponent_type, opponent_strategy):
+        player_index = player_type - 1
+        player_action = player_strategy[player_index]
+
+        opponent_index = opponent_type - 1
+        opponent_action = opponent_strategy[opponent_index]
+
+        return self.utility_values[(player_type, player_action, opponent_type, opponent_action)]
 
 
 class PlayerSpecificationTest(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super(PlayerSpecificationTest, self).__init__(*args, **kwargs)
+        self.sample_game = SampleGame()
+
     def test_pure_strategies(self):
-        player_specification = PlayerSpecification(player_types=[1, 2], player_actions=["U", "D"])
-        expected_strategies = {("U", "U"), ("U", "D"), ("D", "U"), ("D", "D")};
-        actual_strategies = set()
-        for pure_strategy in player_specification.get_pure_strategies():
-            actual_strategies.add(pure_strategy)
+        expected_strategies = {("U", "U"), ("U", "D"), ("D", "U"), ("D", "D")}
+        actual_strategies = get_all_strategies(self.sample_game.player_specification)
 
         self.assertEqual(actual_strategies, expected_strategies)
+
+        expected_strategies = {("R", "R"), ("R", "L"), ("L", "R"), ("L", "L")}
+        actual_strategies = get_all_strategies(self.sample_game.opponent_specification)
+        self.assertEqual(actual_strategies, expected_strategies)
+
+    def test_get_utility(self):
+        expected_opponent_utility = 1.
+        player_strategy = ("U", "U")
+        opponent_strategy = ("L", "L")
+        actual_player_utility, actual_opponent_utility = self.sample_game.get_expected_utilities(
+            (player_strategy, opponent_strategy))
+
+        self.assertAlmostEqual(actual_opponent_utility, expected_opponent_utility)
+
+
+def get_all_strategies(player_specification):
+    actual_strategies = set()
+    for pure_strategy in player_specification.get_pure_strategies():
+        actual_strategies.add(pure_strategy)
+
+    return actual_strategies
