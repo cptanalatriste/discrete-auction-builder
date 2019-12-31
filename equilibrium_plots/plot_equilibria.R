0a65861740242b031cbd4936881648c79b7bdf78
Carlos G. Gavidia <cgavidiac@gmail.com>
2019-12-31 15:19:21 +0000

    Changing background

--- equilibrium_plots/plot_equilibria.R
+++ equilibrium_plots/plot_equilibria.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 
 equilibrium <- read_csv("3_bidders_13_valuations.csv")
-equilibrium <- read_csv("3_bidders_no_ties_7_valuations.csv")
 
 first_equilibrium <- filter(equilibrium, Equilibria == 1)
 
@@ -17,14 +16,15 @@ temp <- rename(temp, bidder_1 = 2)
 temp <- rename(temp, bidder_2 = 3)
 temp <- rename(temp, bidder_3 = 4)
 
-alpha_value <- 0.3
-size_value <- 1.5
+alpha_value <- 0.5
+size_value <- 1.8
 
 ggplot(data = temp) + 
+  theme_bw() +
   xlab("Valuation") + 
   ylab("Bid") +
   geom_step(mapping = aes(x = valuation, y = bidder_1, group = 1), color="red", alpha = alpha_value, size = size_value) +
   geom_step(mapping = aes(x = valuation, y = bidder_2, group = 2), color='blue', alpha = alpha_value, size = size_value) +
-  geom_step(mapping = aes(x = valuation, y = bidder_3, group = 3), color='green',alpha = alpha_value, size = size_value) +
+  geom_step(mapping = aes(x = valuation, y = bidder_3, group = 3), color='darkgreen',alpha = alpha_value, size = size_value) +
   geom_line(mapping = aes(x = valuation, y = cont_bid, group = 4), linetype = "dashed") 
   
