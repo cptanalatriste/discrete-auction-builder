898e9938d0b91e6b1e714d2d04dd82b5d78a3142
Carlos G. Gavidia <cgavidiac@gmail.com>
2019-12-31 13:08:18 +0000

    Including Itzhak's plotting suggestion's

--- equilibrium_plots/plot_equilibria.R
+++ equilibrium_plots/plot_equilibria.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+equilibrium <- read_csv("3_bidders_13_valuations.csv")
 equilibrium <- read_csv("3_bidders_no_ties_7_valuations.csv")
 
 first_equilibrium <- filter(equilibrium, Equilibria == 1)
@@ -8,7 +9,7 @@ temp <- select(first_equilibrium, -Equilibria, -Player)
 temp <- rownames_to_column(temp)
 temp <- gather(temp, var, value, -rowname)
 temp <- spread(temp, rowname, value)
-temp <- mutate(temp, valuation = as.numeric(str_sub(var, nchar(var), nchar(var))))
+temp <- mutate(temp, valuation = as.numeric(str_sub(var, 11, nchar(var))))
 temp <- mutate(temp, cont_bid = valuation * 2.0 / 3.0)
 
 
@@ -16,11 +17,14 @@ temp <- rename(temp, bidder_1 = 2)
 temp <- rename(temp, bidder_2 = 3)
 temp <- rename(temp, bidder_3 = 4)
 
+alpha_value <- 0.3
+size_value <- 1.5
+
 ggplot(data = temp) + 
   xlab("Valuation") + 
   ylab("Bid") +
-  geom_line(mapping = aes(x = valuation, y = bidder_1, group = 1), color="red", alpha = 0.3) +
-  geom_line(mapping = aes(x = valuation, y = bidder_2, group = 2), color='blue', alpha = 0.3) +
-  geom_line(mapping = aes(x = valuation, y = bidder_3, group = 3), color='green',alpha = 0.3) +
+  geom_step(mapping = aes(x = valuation, y = bidder_1, group = 1), color="red", alpha = alpha_value, size = size_value) +
+  geom_step(mapping = aes(x = valuation, y = bidder_2, group = 2), color='blue', alpha = alpha_value, size = size_value) +
+  geom_step(mapping = aes(x = valuation, y = bidder_3, group = 3), color='green',alpha = alpha_value, size = size_value) +
   geom_line(mapping = aes(x = valuation, y = cont_bid, group = 4), linetype = "dashed") 
   
